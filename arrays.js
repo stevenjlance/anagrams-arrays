4a47d04d64694619adc5050e4a8686b3e05a8948
Glitch (anagrams-array-apcsp) <none>
2020-06-26 18:37:37 +0000

    🎥👸 Updated with Glitch

--- arrays.js
+++ arrays.js
@@ -8,44 +8,37 @@ let anagrams = ["plots", "post", "stop", "puts", "tops"]
 
 // 1. Print out the third word in the array using bracket notation. This shoud print the word "stop".
 
-console.log(anagrams[2]);
+
 
 
 
 
 // 2. Print out the length of the list
-console.log(anagrams.length)
+
 
 
 
 
 // 3. The word "opts" is also an anagram of the word "stop". Find a way to add "opts" to the end of the array. HINT: Use this documentation - https://www.w3schools.com/jsref/jsref_push.asp.
-anagrams.push('opts');
+
 
 
 // 4. The word "puts", on the other hand, is not an anagram of the word "stop". Find a way to replace it with the word "pots".
-anagrams[3] = 'pots';
+
 
 
 
 
 // 5. Use the documentation to figure out what the methods ".pop()" and ".shift()" do.
 //   Now use one of them to remove the word "plots" (which isn't a correct anagram of "stop") from our array.
-console.log(anagrams);
-// Removes the last item
-// anagrams.pop()
-// Removes the first item
-anagrams.shift();
-console.log(anagrams);
+
 
 
 // 6. Put the final array of anagrams in alphabetical order.
-anagrams.sort();
-console.log(anagrams);
 
 
-// This code prints the array after you've manipulated it above.
-console.log(anagrams)
+
+
 
 // Before moving on to level 2, you will probably want to comment it out. 
 
@@ -57,17 +50,17 @@ console.log(anagrams)
 // Pro-tip: to make this easier, scroll down and comment out the line "console.log(anagrams)" so that you aren't printing extra information.
 
 // 7. The first array is called "fortunes" and contains fortune-cookie style fortunes. Print out the third fortune from the array.
-console.log(fortunes[2])
+
 
 
 
 // 8. This array is huge, so it'd be helpful to know how many fortunes are listed. Find a way to print out the number of fortunes in the array.
-console.log(fortunes.length)
+
 
 
 
 // 9. Print out a random fortune from the array.
-console.log(fortunes[Math.floor(Math.random()*fortunes.length)])
+
 
 
 
